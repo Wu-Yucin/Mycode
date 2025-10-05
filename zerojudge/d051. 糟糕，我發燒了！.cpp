bbb5cc4d787b9724d30211ceaeba0f5a70c8bb18
Wu-Yucin <wuy9812@gmail.com>
2025-10-05 10:57:52 +0800

    Update d051. 糟糕，我發燒了！.cpp

--- zerojudge/d051. 糟糕，我發燒了！.cpp
+++ zerojudge/d051. 糟糕，我發燒了！.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//f:µØ¤ó·Å«×
+//f:華氏溫度
 int f;
 int main(){
     cin >> f;
-    cout << (f-32)*5/9;
+    cout <<fixed <<setprecision(3);
+    cout << (f-32)*((double)5/9);
     return 0;
 }
