b24f57a78e37c12b2d10f4275871f2308269a51d
Wu-Yucin <wuy9812@gmail.com>
2025-11-03 15:20:58 +0800

    Create p905. 找數字 (Numbers).cpp

---
+++ zerojudge/p905. 找數字 (Numbers).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//n:正整數,is_prime:是否為質數
+int n;
+bool is_prime=true;
+int main(){
+    cin >> n;
+    for(int i=n ; i>0 ; ++i){
+        if (%i == 0){
+            cout << i;
+            break;
+        }
+    }
+    for (int j=n ; j>0 ; ++j){
+        if (sqrt(j) = int){
+            cout << j;
+            break;
+        }
+    }
+    for (int w=n ; w>0 ; ++w){
+        if (sqrt(sqrt(w)) = int){
+            cout << w;
+            break;
+        }
+    }
+    return 0;
+}
