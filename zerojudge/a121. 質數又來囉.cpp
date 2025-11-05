ea1f9ba2c63e53a34316b9fee5d88572f4b1f456
Wu-Yucin <wuy9812@gmail.com>
2025-11-05 20:27:12 +0800

    Update a121. 質數又來囉.cpp

--- zerojudge/a121. 質數又來囉.cpp
+++ zerojudge/a121. 質數又來囉.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 //a,b:正整數;is_prime:是否是質數;n:幾個質數
-int a,b,n=0;
-bool is_prime = true;
+int a , b , n;
+bool is_prime =true;
 int main(){
     while(cin >> a >> b){
+        n = 0;
         for (int i=a ; i<=b ; ++i){
+            is_prime = true;
             for (int j=2 ; j<=sqrt(i) ; ++j){
                 if (i%j==0){
                     is_prime=false;
