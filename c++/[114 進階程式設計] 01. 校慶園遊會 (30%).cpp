4e38d8636f0028334cf19010098dd884354f66cc
Wu-Yucin <wuy9812@gmail.com>
2025-11-15 17:21:11 +0800

    Create [114 進階程式設計] 01. 校慶園遊會 (30%).cpp

---
+++ c++/[114 進階程式設計] 01. 校慶園遊會 (30%).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+///c:商品名稱,m:商品價格
+string c;
+int m;
+int main(){
+    cin >> c ;
+    cin >> m;
+    cout << "vvvvvvvvvvvvvvv" << endl;
+    cout << ">   !!SALE!!" << endl;
+    cout << ">   " << c << endl;
+    cout << ">   NTD " << m << endl;
+    cout <<"^^^^^^^^^^^^^^^" << endl;
+    return 0;
+}
