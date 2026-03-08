c77522044662ae88780dc0c6c696e10c6f59e6e1
Wu-Yucin <wuy9812@gmail.com>
2026-03-08 14:23:16 +0800

    Update e799. p6. 資工系的浪漫.cpp

--- zerojudge/e799. p6. 資工系的浪漫.cpp
+++ zerojudge/e799. p6. 資工系的浪漫.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+
+///n:高,m:寬,c:二進位中1的輸出,s:十進位數字
+int main(){
+    long long n, m, s;
+    char c;
+    cin >> n >> m >> c;
+    for (int i = 0; i < n; ++i){
+        cin >> s;
+        char board[100];
+        for (int j = 0; j < m; ++j) {
+            board[j] = '.';
+        }
+        int p = m - 1;
+        while (s > 0 && p >= 0) {
+            if (s % 2 == 1) {
+                board[p] = c;
+            }
+            s /= 2;
+            p--;
+        }
+        for (int j = 0; j < m; ++j) {
+            cout << board[j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
