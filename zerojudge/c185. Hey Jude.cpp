e51a6b0b38492576c7ff1e688e9f9bb2734e4b02
Wu-Yucin <wuy9812@gmail.com>
2026-03-18 15:48:30 +0800

    Create c185. Hey Jude.cpp

---
+++ zerojudge/c185. Hey Jude.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    getline(cin,s);
+    cout << "Hey " << s;
+    return 0;
+}
