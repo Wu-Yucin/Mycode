ec4005200800008d3fb59ae7e105c70a75e8c118
Wu-Yucin <wuy9812@gmail.com>
2026-03-18 16:02:06 +0800

    Create e524. 106 彰雲嘉區複賽 - Q4 變位字判斷.cpp

---
+++ zerojudge/e524. 106 彰雲嘉區複賽 - Q4 變位字判斷.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i=0;i<n;++i){
+        getline(cin,s1);
+        getline(cin,s2);
+
+        for (int j=0;j<s2.size();++j){
+
+        }
+    }
+    return 0;
+}
