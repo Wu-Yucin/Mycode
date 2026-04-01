8fc6f9cf19de024be460d704f241e773065f63cd
Wu-Yucin <wuy9812@gmail.com>
2026-04-01 12:33:20 +0800

    Create e927. pA. 字串排序.cpp

---
+++ zerojudge/e927. pA. 字串排序.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin >> s;
+    sort(s.begin(),s.end());
+    cout << s;
+    return 0;
+}
+
